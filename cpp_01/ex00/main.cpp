a82d0f2a0ea3824d6ff908c098b3c3dda09106dd
Markus Korpela <mkorpela@c1r5p21.hive.fi>
2024-09-23 14:20:12 +0300

    made class, get_name, set_name, started function that makes a zombie

--- cpp_01/ex00/main.cpp
+++ cpp_01/ex00/main.cpp
@@ -1,9 +1,57 @@
 
+#include <iostream>
+#include <string>
 
-/*
-	-Bt
-*/
+class Zombie
+{
+	public:
+		Zombie();
+		~Zombie();
+		
+		void 		announce(void);//const - add into all functions....
+		void 		set_name(std::string);
+		std::string get_name(void); 
 
+	private:
+		std::string name;
+};
+
+Zombie::Zombie()
+{
+
+}
+
+Zombie::~Zombie()
+{
+
+}
+
+
+void		Zombie::set_name(std::string input)
+{
+	this->name = input;
+}
+
+std::string 	Zombie::get_name(void)
+{
+	return (this->name);
+}
+
+void	Zombie::announce(void)
+{
+	std::cout << get_name();
+	std::cout << ": ";
+	std::cout << "BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie* newZombie(std::string name)
+{
+	Zombie newZombie;//Do I need a pointer here???
+	
+	newZombie.set_name("hiiiii");//new
+	newZombie.announce();
+	return (&newZombie);
+}
 
 /*
 	-zombie.hpp
@@ -11,10 +59,14 @@
 	2 functions
 		-Zombie* newZombie( std::string name );
 		-void randomChump( std::string name );
+			-so they are not part of the zombie class???
 */
 
 int main(void)
 {
+	Zombie z1;
+
+
 	return (0);
 }
 
