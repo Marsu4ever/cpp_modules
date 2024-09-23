5ac5a41f763aab6e238b4e91649ca8e934732612
Markus Korpela <mkorpela@c1r5p21.hive.fi>
2024-09-23 13:59:28 +0300

    started cpp_01: Makefile and main.c

---
+++ cpp_01/ex00/main.cpp
@@ -0,0 +1,23 @@
+
+
+/*
+	-Bt
+*/
+
+
+/*
+	-zombie.hpp
+
+	2 functions
+		-Zombie* newZombie( std::string name );
+		-void randomChump( std::string name );
+*/
+
+int main(void)
+{
+	return (0);
+}
+
+/*
+	-
+*/
