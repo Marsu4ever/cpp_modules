db1816dd306de077cdd2efee0171148de105dd3b
Markus Korpela <mkorpela@c1r6p12.hive.fi>
2024-10-21 16:08:54 +0300

    sumting was chnged...

--- cpp_04/ex02/main.cpp
+++ cpp_04/ex02/main.cpp
@@ -2,22 +2,13 @@
 #include "Cat.hpp"
 #include "Dog.hpp"
 
-/*
-	-
-	-
-	-
-
-*/
-
-
-
 int main(void)
 {
-
 	Dog dog;
 	(void)dog;
-
-	/* Test - Uncomment below and read what compiler says */
+	
+	/* Test - Uncomment the line below and read what compiler says ;) */
+	
 	// AAnimal animal;
 
 	return (0);
