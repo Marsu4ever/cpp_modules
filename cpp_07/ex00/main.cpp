9995bd62feacf3780c46bb2cc2be6d8cb5d61fbf
Markus Korpela <markuskristiankorpela@gmail.com>
2025-02-04 10:19:13 +0200

    Deleted comments and exercise notes

--- cpp_07/ex00/main.cpp
+++ cpp_07/ex00/main.cpp
@@ -1,48 +1,4 @@
 
-/*
-	Is it a program?	-> Yes. it has a main.cpp
-
-	swap - Swaps the values of two given arguments. Does not return anything.
-	min	- 	Compares the two values passed in its arguments and returns the smallest one.
-			If the two of them are equal, then it returns the second one.
-	max	-	Compares the two values passed in its arguments and returns the greatest one.
-			If the two of them are equal, then it returns the second one.
-
-	can call funcs with any argument
-		-arguments HAVE to be SAME type
-		-must support ALL comparison operators
-
-	-templates defined in Header file
-
-
-	int
-	char
-	double
-	float
-
-	string
-	Pointers?
-	Strange Objects?
-*/
-
-
-
-
-
-/*
-	#include <iostream>
-
-	template <typename T>
-	T maxValue(T a, T b) {
-		return (a > b) ? a : b;
-	}
-
-	int main() {
-    std::cout << maxValue(5, 10) << std::endl;    // Works with int
-    std::cout << maxValue(3.5, 2.1) << std::endl; // Works with double
-}
-*/
-
 #include "my_first_templates.hpp"
 #include <limits> 	//	max float
 
@@ -80,20 +36,3 @@ int main(void)
 
 	return (0);
 }
-
-// int
-// main( void ) {
-// 	int a = 2;
-// 	int b = 3;
-// 	::swap( a, b );
-// 	std::cout << "a = " << a << ", b = " << b << std::endl;
-// 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-// 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-// 	std::string c = "chaine1";
-// 	std::string d = "chaine2";
-// 	::swap(c, d);
-// 	std::cout << "c = " << c << ", d = " << d << std::endl;
-// 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-// 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-// 	return 0;
-// }
